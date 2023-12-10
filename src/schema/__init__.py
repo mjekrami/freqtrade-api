395f0f4edf321c731dc2902cb4714e23841d6dbf
mjekrami <mjavad.ekrami@gmail.com>
2023-12-11 01:43:50 +0330

    added /performance endpoint and schema

--- src/schema/__init__.py
+++ src/schema/__init__.py
@@ -2,3 +2,4 @@ from .container import ContainerSchema
 from .trade import *
 from .profit import ProfitSchema, BotProfitSchema
 from .stats import StatSchema, BotStatSchema, TimeUnit
+from .performance import PerformanceSchema, BotPerformanceSchema
